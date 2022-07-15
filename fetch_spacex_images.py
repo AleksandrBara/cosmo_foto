66789df31d923e3d152c68dd484b6deceaa34c7a
AleksandrBara <alexbar130379@gmail.com>
2022-07-15 14:32:05 +0300

    Устранил замечпния

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,6 +2,14 @@ import requests
 import urllib.parse
 import os
 import argparse
+import datetime
+from pathlib import Path
+
+
+def make_time_stamp():
+    current_time = datetime.datetime.now()
+    time_stamp = current_time.strftime("%Y%m%d%H%M%S")
+    return time_stamp
 
 
 def get_args():
@@ -11,11 +19,11 @@ def get_args():
     return launch_number
 
 
-def make_file_name_from_link(link):
+def make_file_extension_from_link(link):
     url_component = urllib.parse.urlparse(link).path
     url_component_clean = urllib.parse.unquote(url_component)
-    file_name = os.path.split(url_component_clean)[1]
-    return file_name
+    file_extension = os.path.splitext(url_component_clean)[1]
+    return file_extension
 
 
 def fetch_spacex_any_launch(launch_number):
@@ -30,20 +38,27 @@ def fetch_spacex_any_launch(launch_number):
 
 if __name__ == '__main__':
     directory_name = 'space_images'
-    if not os.path.exists(directory_name):
-        os.mkdir(directory_name)
+    os.makedirs(directory_name, exist_ok=True)
     launch_number = get_args()
     try:
         links = fetch_spacex_any_launch(launch_number)
-        for link in links:
-            file_name = make_file_name_from_link(link)
-            file_path = f'{directory_name}/{file_name}'
-            response = requests.get(link)
-            response.raise_for_status()
-            with open(file_path, 'wb') as file:
-                file.write(response.content)
     except (
             requests.exceptions.HTTPError,
             requests.exceptions.ConnectionError
     ) as e:
         print('Не возможно получить данные с сервера:\n{}'.format(e))
+
+    for link in links:
+        file_extension = make_file_extension_from_link(link)
+        file_name = f"'spacex'{make_time_stamp()}{file_extension}"
+        file_path = Path(directory_name, file_name)
+        try:
+            response = requests.get(link)
+            response.raise_for_status()
+        except (
+                requests.exceptions.HTTPError,
+                requests.exceptions.ConnectionError
+        ) as e:
+            print('Не возможно получить данные с сервера:\n{}'.format(e))
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
