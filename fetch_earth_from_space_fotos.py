04823e8ba1805dc864a57dca4a611c0a09bffb24
AleksandrBara <alexbar130379@gmail.com>
2022-07-15 15:31:29 +0300

    Изменил логику получения имени файла.

--- fetch_earth_from_space_fotos.py
+++ fetch_earth_from_space_fotos.py
@@ -1,45 +1,40 @@
-import urllib.parse
 import requests
-import urllib.parse
+from pathlib import Path
 import os
 
 
-def make_file_name_from_link(link):
-    url_component = urllib.parse.urlparse(link).path
-    url_component_clean = urllib.parse.unquote(url_component)
-    file_name = os.path.split(url_component_clean)[1]
-    return file_name
-
-
-def fetch_earth_from_space_fotos():
+def get_links_and_all_id_from_nasa():
     url = "https://epic.gsfc.nasa.gov/api/natural"
     payload = {}
     headers = {}
     response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     fotos_descriptions = response.json()
-    links = list()
+    links_and_all_id = list()
     for fotos_description in fotos_descriptions:
         image_id = str(fotos_description['image'])
         day_id = str(fotos_description['date'])
         day_id, _ = day_id.replace('-', '/').split(' ')
         link = f'https://epic.gsfc.nasa.gov/archive/natural/{day_id}/png/{image_id}.png'
-        links.append(link)
-    return links
+        link_and_id = (link, image_id)
+        links_and_all_id.append(link_and_id)
+    return links_and_all_id
 
 
 if __name__ == '__main__':
     directory_name = 'space_images'
-    if not os.path.exists(directory_name):
-        os.mkdir(directory_name)
+    os.makedirs(directory_name, exist_ok=True)
     try:
-        links = fetch_earth_from_space_fotos()
-        for link in links:
-            file_name = make_file_name_from_link(link)
-            file_path = f'{directory_name}/{file_name}'
-            response = requests.get(link)
-            response.raise_for_status()
-            with open(file_path, 'wb') as file:
-                file.write(response.content)
+        links = get_links_and_all_id_from_nasa()
     except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
         print('Не возможно получить данные с сервера:\n{}'.format(e))
+    for link, image_id in links:
+        file_name = f"{image_id}.png"
+        file_path = Path(directory_name, file_name)
+        try:
+            response = requests.get(link)
+            response.raise_for_status()
+        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+            print('Не возможно получить данные с сервера:\n{}'.format(e))
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
