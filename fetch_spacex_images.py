9d23266f81fe20aae92120393b4cc1272c402de9
AleksandrBara <alexbar130379@gmail.com>
2022-07-17 16:13:52 +0300

    Сократил блоки "try"

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -6,7 +6,7 @@ from secondary_function import make_time_stamp
 from secondary_function import make_file_extension_from_link
 
 
-def get_args():
+def get_lounch_number_from_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--launch_number", default=55)
     launch_number = parser.parse_args().launch_number
@@ -20,32 +20,24 @@ def fetch_spacex_any_launch(launch_number):
     response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     links = response.json()['links']['flickr_images']
-    return links
+    for link in links:
+        file_extension = make_file_extension_from_link(link)
+        file_name = f"'spacex'{make_time_stamp()}{file_extension}"
+        file_path = Path(directory_name, file_name)
+        response = requests.get(link)
+        response.raise_for_status()
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
     directory_name = 'space_images'
     os.makedirs(directory_name, exist_ok=True)
-    launch_number = get_args()
+    launch_number = get_lounch_number_from_args()
     try:
         links = fetch_spacex_any_launch(launch_number)
     except (
             requests.exceptions.HTTPError,
             requests.exceptions.ConnectionError
     ) as e:
-        quit('Не возможно получить данные с сервера:\n{}'.format(e))
-
-    for link in links:
-        file_extension = make_file_extension_from_link(link)
-        file_name = f"'spacex'{make_time_stamp()}{file_extension}"
-        file_path = Path(directory_name, file_name)
-        try:
-            response = requests.get(link)
-            response.raise_for_status()
-        except (
-                requests.exceptions.HTTPError,
-                requests.exceptions.ConnectionError
-        ) as e:
-            print('Не возможно получить данные с сервера:\n{}'.format(e))
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
+        print('Не возможно получить данные с сервера:\n{}'.format(e))
--- get_astronomy_picture_of_the_day.py
+++ get_astronomy_picture_of_the_day.py
@@ -7,7 +7,7 @@ from secondary_function import make_time_stamp
 from secondary_function import make_file_extension_from_link
 
 
-def get_args():
+def get_counter_from_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--count", default=5)
     count = parser.parse_args().count
@@ -15,9 +15,6 @@ def get_args():
 
 
 def get_astronomy_picture_of_the_day(token, foto_count):
-    directory_name = 'space_images'
-    if not os.path.exists(directory_name):
-        os.mkdir(directory_name)
     headers = {}
     payload = ('api_key', token), ('count', foto_count)
     response = requests.get(
@@ -27,37 +24,27 @@ def get_astronomy_picture_of_the_day(token, foto_count):
     )
     response.raise_for_status()
     foto_discriptions = response.json()
-    links = list()
     for foto_discription in foto_discriptions:
         link = foto_discription['hdurl']
-        links.append(link)
-    return links
+        file_extension = make_file_extension_from_link(link)
+        file_name = f"'NASA'{make_time_stamp()}{file_extension}"
+        file_path = Path(directory_name, file_name)
+        response = requests.get(link)
+        response.raise_for_status()
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
-    directory_name = 'space_images'
-    os.makedirs(directory_name, exist_ok=True)
     load_dotenv()
     token = os.getenv("NASA_TOKEN")
-    foto_count = get_args()
+    directory_name = 'space_images'
+    os.makedirs(directory_name, exist_ok=True)
+    foto_count = get_counter_from_args()
     try:
         links = get_astronomy_picture_of_the_day(token, foto_count)
     except (
             requests.exceptions.HTTPError,
             requests.exceptions.ConnectionError
     ) as e:
-        quit('Не возможно получить данные с сервера:\n{}'.format(e))
-    for link in links:
-        file_extension = make_file_extension_from_link(link)
-        file_name = f"'NASA'{make_time_stamp()}{file_extension}"
-        file_path = Path(directory_name, file_name)
-        try:
-            response = requests.get(link)
-            response.raise_for_status()
-        except (
-                requests.exceptions.HTTPError,
-                requests.exceptions.ConnectionError
-        ) as e:
-            print('Не возможно получить данные с сервера:\n{}'.format(e))
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
+        print('Не возможно получить данные с сервера:\n{}'.format(e))
