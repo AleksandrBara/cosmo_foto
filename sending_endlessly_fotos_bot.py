63bae2f7fb0e9a3340ce7c2f50da966b1caf6a30
AleksandrBara <alexbar130379@gmail.com>
2022-07-18 21:40:57 +0300

    Изменил функцию send_pictures_to_telegram_endlessly()

--- sending_endlessly_fotos_bot.py
+++ sending_endlessly_fotos_bot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import argparse
 from time import sleep
 from secondary_function import get_file_paths_from_directory
+from secondary_function import send_picture_to_telegram
 
 
 def get_time_out_from_args():
@@ -14,6 +15,28 @@ def get_time_out_from_args():
     return time_out
 
 
+def send_pictures_to_telegram_endlessly(token, chat_id, file_paths):
+    for file_path in file_paths:
+        try:
+            send_picture_to_telegram(token, chat_id, file_path)
+            sleep(int(time_out))
+        except (telegram.error.Unauthorized, telegram.error.InvalidToken) as e:
+            quit('Проверьте ваши данные! \n Ошибка:\n{}'.format(e))
+        except telegram.error.NetworkError as e:
+            print('Отсутствует подключение! \n Ошибка:\n{}'.format(e))
+            sleep(600)
+    while True:
+        random.shuffle(file_paths)
+        for file_path in file_paths:
+            try:
+                send_picture_to_telegram(token, chat_id, file_path)
+                sleep(int(time_out))
+            except (telegram.error.Unauthorized, telegram.error.InvalidToken) as e:
+                quit('Проверьте ваши данные! \n Ошибка:\n{}'.format(e))
+            except telegram.error.NetworkError as e:
+                print('Отсутствует подключение! \n Ошибка:\n{}'.format(e))
+                sleep(600)
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -22,20 +45,4 @@ if __name__ == '__main__':
     time_out = get_time_out_from_args()
     directory = 'space_images'
     file_paths = get_file_paths_from_directory(directory)
-    bot = telegram.Bot(token=token)
-    try:
-        for file_path in file_paths:
-            with open(file_path, 'rb') as file:
-                bot.send_document(chat_id=chat_id, document=file)
-            sleep(int(time_out))
-    except (telegram.error.NetworkError, telegram.error.InvalidToken) as e:
-        print('Ошибка:\n{}'.format(e))
-    while True:
-        random.shuffle(file_paths)
-        try:
-            for file_path in file_paths:
-                with open(file_path, 'rb') as file:
-                    bot.send_document(chat_id=chat_id, document=file)
-                sleep(int(time_out))
-        except (telegram.error.NetworkError, telegram.error.InvalidToken) as e:
-            print('Ошибка:\n{}'.format(e))
+    send_pictures_to_telegram_endlessly(token, chat_id, file_paths)
